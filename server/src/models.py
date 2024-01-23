b43338e7e6f751ff07d8e03f95fdfbc507923e7f
ValmirNogFilho <valmirnogueira514@gmail.com>
2024-01-23 19:33:10 -0300

    :sparkles: feat: creating model Task

--- server/src/models.py
+++ server/src/models.py
@@ -1,4 +1,26 @@
 from src import db
 
 class Task(db.Model):
-    pass
+
+    id: int = db.Column(db.Integer(), primary_key=True)
+    title: str = db.Column(db.String(length=80), nullable=False)
+    description: str = db.Column(db.String(length=500), nullable=False)
+    status: str = db.Column(db.String(length=10), nullable=False)
+
+    def __init__(self, title: str, description: str, status: str):
+        self.title = title
+        self.description = description
+        self.status = status
+
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "status": self.status,
+        }
+    
+    def update(self, updated_task_request: dict):
+        self.title = updated_task_request["title"]
+        self.description = updated_task_request["description"]
+        self.status = updated_task_request["status"]
