42eaece3861be887bccccdb9373c644dbb714129
ValmirNogFilho <valmirnogueira514@gmail.com>
2024-01-24 18:56:06 -0300

    :recycle: refactor: moving all server directory to new repo

--- client/src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Button.css'
-const Button = ({onclick}) => {
-  return (
-    <button className='add-task-btn' onClick={onclick}><h1>Adicionar nova tarefa</h1></button>
-  )
-}
-
-export default Button
